6abd7ed6244fa4fbdc736a0f24c7070053336ee9
MUZAFFER SINA ACIKGOZ <TCMUACIKGOZ@turkcell.entp.tgc>
2022-06-23 11:09:23 +0300

    .

---
+++ apacheKafka/producerr/src/test/java/com/kafkademo/producerr/ProducerrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkademo.producerr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
